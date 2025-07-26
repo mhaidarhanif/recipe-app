cdad62bf4c504b9bb73e9940b85db892e8a4638e
M Haidar Hanif <git@mhaidarhanif.com>
2025-07-26 21:48:58 +0700

    feat: recipe component

--- src/app.tsx
+++ src/app.tsx
@@ -3,6 +3,16 @@ export function App() {
     <div>
       <h1>Recipe App</h1>
       <button>Click me</button>
+
+      <Recipe />
+    </div>
+  );
+}
+
+export function Recipe() {
+  return (
+    <div>
+      <h2>Fried Rice</h2>
     </div>
   );
 }
