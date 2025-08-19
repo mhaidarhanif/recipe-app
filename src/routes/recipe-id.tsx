0ec97187a3ff3bb9619968e880e87918714d90d6
M Haidar Hanif <git@mhaidarhanif.com>
2025-08-19 22:46:20 +0700

    feat: get recipe by id

--- src/routes/recipe-id.tsx
+++ src/routes/recipe-id.tsx
@@ -1,3 +1,4 @@
+import type { DataRecipe } from "@/modules/recipe/data";
 import { useParams } from "react-router";
 
 export function RecipeId() {
@@ -5,9 +6,22 @@ export function RecipeId() {
 
   const id = Number(params.id);
 
+  const storedRecipes = localStorage.getItem("recipes") || "[]";
+  const recipes = JSON.parse(storedRecipes) as DataRecipe[];
+  const recipe = recipes.find((recipe) => recipe.id === id);
+
+  if (!recipe) {
+    return (
+      <div>
+        <p>Recipe not found</p>
+      </div>
+    );
+  }
+
   return (
     <div>
-      <h1>Recipe: {id}</h1>
+      <h1>{recipe.name}</h1>
+      <p>{recipe.description}</p>
     </div>
   );
 }
